6757a9b7af7d293523bba671601411c4f1ec6a15
celineeebeanie <parkm4@bxscience.edu>
2016-03-05 21:41:04 -0500

    3.5.16

---
+++ Librarian.java
@@ -0,0 +1,32 @@
+public class Librarian{
+  
+  public void addBook(Book book){
+    FileHandler.writeFile("Books.txt", book.toString());
+  }
+  
+  public void removeBook(Book book){
+    String[] books = FileHandler.readFile("Books.txt");
+    
+    int counter = 1;
+    for (String singleBook: books){
+      if (singleBook.equals(book.toString())){
+        FileHandler.removeLine("Books.txt", counter);
+        System.out.println("\tSuccesfuly Removed " + FileHandler.readLine(singleBook, 2));
+        break;
+      }
+    }
+  }
+  
+  public String getBorrowingHistory(String bookName){
+    String[] borrowingHistory = FileHandler.readFile("BorrowingHistory.txt");
+    
+    for(String line: borrowingHistory){
+      System.out.println(FileHandler.readLine(line, 2));
+      if (FileHandler.readLine(line, 2).equals(bookName) || FileHandler.readLine(line, 1).equals(bookName)){
+        return line;
+      }
+    }
+    
+    return "No History Found";
+  }
+}
